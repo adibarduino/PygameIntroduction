d94195a994340dcb88fdcf764c24321ed02a7193
MathEX <66177113+adibarduino@users.noreply.github.com>
2020-06-07 09:14:58 +0100

    Update and rename #1 IntroPygame v1.py to #1 IntroPygame v2.py

--- #1 IntroPygame v2.py
+++ #1 IntroPygame v2.py
@@ -1,4 +1,5 @@
 """
+MatheX, le 07 juin 2020
 Série d'introduction à Pygame
 Prérequis:
     - les bases de Python
@@ -16,21 +17,20 @@ import time
 import random
 
 # déclaration des constantes
-bleu = (0,0,255) # (R,G,B) entre 0 et 255
-rouge = (255, 0, 0) # (R,G,B) entre 0 et 255
-noir = (0,0,0) # (R,G,B) entre 0 et 255
-blanc = (255,255,255) # (R,G,B) entre 0 et 255
+bleu = (0, 0, 255)  # (R,G,B) entre 0 et 255
+rouge = (255, 0, 0)  # (R,G,B) entre 0 et 255
+noir = (0, 0, 0)  # (R,G,B) entre 0 et 255
+blanc = (255, 255, 255)  # (R,G,B) entre 0 et 255
 pas = 10
 
 # initialise Pygame
 pygame.init()
 
-
 # création de la fenêtre principale du jeu
-window = pygame.display.set_mode( (500,500))
+window = pygame.display.set_mode((500, 500))
 
 # création du joueur
-player = pygame.draw.rect(window , bleu , (100,100,20,20))
+player = pygame.draw.rect(window, bleu, (100, 100, 20, 20))
 # création du méchant
 enemy = pygame.draw.rect(window, rouge, (300, 300, 20, 20))
 
@@ -52,14 +52,13 @@ while run:
     # mouvement du joueur
     K_Pressed = pygame.key.get_pressed()
     if K_Pressed[pygame.K_LEFT]:
-        player.move_ip(-pas,0)
+        player.move_ip(-pas, 0)
     elif K_Pressed[pygame.K_RIGHT]:
-        player.move_ip(pas,0)
+        player.move_ip(pas, 0)
     elif K_Pressed[pygame.K_UP]:
-        player.move_ip(0,-pas)
+        player.move_ip(0, -pas)
     elif K_Pressed[pygame.K_DOWN]:
-        player.move_ip(0,pas)
-
+        player.move_ip(0, pas)
 
     # mis à jour de la couleur de la fenêtre principale
     window.fill(noir)
@@ -74,8 +73,6 @@ while run:
 
     # mis à jour de l'affichage
     pygame.display.update()
-
-
 
 # quitte Pygame
 pygame.quit()
